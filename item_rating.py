5990147aba8b40ea2d8369b1ebb29fadcc1d7ab2
harshita-dhakad <harshitadhakad1204@gmail.com>
2025-03-11 14:11:07 +0530

    add orderid in item rating

--- item_rating.py
+++ item_rating.py
@@ -7,8 +7,8 @@ order_list_df = pd.read_csv("order_list.csv")
 # Generating random item ratings with 70% Like and 30% Dislike
 order_list_df["rating_item"] = np.random.choice(["Like", "Dislike"], size=len(order_list_df), p=[0.7, 0.3])
 
-# Selecting the required columns for the item rating table
-item_rating_table = order_list_df[["item_id", "user_id", "date", "rating_item"]]
+# Selecting the required columns for the item rating table, including order_id
+item_rating_table = order_list_df[["orderId", "item_id", "user_id", "date", "rating_item"]]
 
 # Save the item rating table to a new CSV file
 item_rating_table.to_csv("item_rating_table.csv", index=False)
@@ -22,4 +22,5 @@ rating_counts = item_rating_table["rating_item"].value_counts()
 # Display the counts
 print("Rating Counts:")
 print(rating_counts)
+
 
